d4828efc6e342e4e316bdcdfd85f621de1e0c70b
link1183 <adrien.gunther@gmail.com>
2024-09-23 13:24:25 +0200

    Updated dump

--- dump.sql
+++ dump.sql
@@ -1,28 +1,16 @@
---
--- Table structure for table `boot`
---
+CREATE TABLE IF NOT EXISTS `users` (
+	`id` int AUTO_INCREMENT NOT NULL UNIQUE,
+	`discord_id` bigint NOT NULL UNIQUE,
+	PRIMARY KEY (`id`)
+);
 
-DROP TABLE IF EXISTS `boot`;
-CREATE TABLE `boot` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `user_id` int(11) NOT NULL,
-  `score` double NOT NULL,
-  `date` datetime NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `id` (`id`),
-  KEY `boot_fk1` (`user_id`),
-  CONSTRAINT `boot_fk1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+CREATE TABLE IF NOT EXISTS `boot` (
+	`id` int AUTO_INCREMENT NOT NULL UNIQUE,
+	`user_id` int NOT NULL,
+	`score` double NOT NULL,
+	`date` datetime NOT NULL,
+	PRIMARY KEY (`id`)
+);
 
---
--- Table structure for table `users`
---
 
-DROP TABLE IF EXISTS `users`;
-CREATE TABLE `users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `discord_id` bigint(20) unsigned NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `id` (`id`),
-  UNIQUE KEY `discord_id` (`discord_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+ALTER TABLE `boot` ADD CONSTRAINT `boot_fk1` FOREIGN KEY (`user_id`) REFERENCES `users`(`id`);
