98a2217593b4447d52f09229bbff32f294e1349e
link1183 <adrien.gunther@gmail.com>
2024-09-25 11:12:33 +0200

    changed msg

--- src/discord/cogs/messages/mod.rs
+++ src/discord/cogs/messages/mod.rs
@@ -18,10 +18,14 @@ pub async fn submit(ctx: Context<'_>, message: String) -> Result<(), Error> {
         let new_user = create_user(conn.clone(), ctx.author().id.get()).await;
         create_message(conn.clone(), new_user, message.clone()).await;
     } else {
-        create_message(conn.clone(), user.unwrap(), message).await;
+        create_message(conn.clone(), user.unwrap(), message.clone()).await;
     };
 
-    ctx.say("Welcome message successfully registered").await?;
+    ctx.say(format!(
+        "The welcome message \"{}\"successfully registered",
+        &message
+    ))
+    .await?;
 
     Ok(())
 }
