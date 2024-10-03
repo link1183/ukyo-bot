c97572bbb1542896ca47d3cea225467b6b6e95ce
link1183 <adrien.gunther@gmail.com>
2024-10-03 17:14:06 +0200

    .

--- mysql/init_scripts/01-Users.sql
+++ mysql/init_scripts/01-Users.sql
@@ -1,10 +1,10 @@
 -- Admin user
 CREATE USER 'admin'@'%' IDENTIFIED BY 'nFo4nAs53?jSJAnS';
-GRANT ALL PRIVILEGES ON *.* TO 'admin_user'@'%' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO 'admin_user'@'%';
 
 -- Exporter user with specific permissions
 CREATE USER 'exporter'@'localhost' IDENTIFIED BY 'exportneptpk' WITH MAX_USER_CONNECTIONS 3;
-GRANT PROCESS, REPLICATION CLIENT, SELECT ON *.* TO 'exporter'@'localhost';
+GRANT PROCESS, REPLICATION CLIENT, SELECT ON *.* TO 'exporter'@'%';
 
 -- Apply changes
 FLUSH PRIVILEGES;
